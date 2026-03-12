857397a671aad85887340f3a83aa5ccdcd64d96b
Tschoppsilas <silas.tschopp@breitband.ch>
2026-03-12 15:23:26 +0100

    neue ps1 dateien

--- commit_and_push.ps1
+++ commit_and_push.ps1
@@ -1,21 +1,27 @@
 param(
-    [string]$msg="update data"
+    [string]$msg="update data",
+    [string]$remote="storage_local"
 )
 
+# --- DVC Änderungen hinzufügen ---
 Write-Host "Adding DVC changes..."
 dvc add Daten
 
+# --- Git Änderungen hinzufügen ---
 Write-Host "Adding Git files..."
 git add Daten.dvc
 git add .gitignore
 # git add .dvc/config  <-- NICHT mehr hinzufügen
 
+# --- Git Commit ---
 Write-Host "Committing..."
 git commit -m $msg
 
+# --- DVC Push ---
 Write-Host "Pushing data to DVC remote..."
-dvc push
+dvc push -r $remote
 
+# --- Git Push ---
 Write-Host "Pushing code to Git..."
 git push
 
