15e419f162c93587e944f030679dacdd8287b6b1
Tschoppsilas <silas.tschopp@breitband.ch>
2026-03-31 09:50:16 +0200

    Pull file angepasst, dass es gelöschte Ordner überall übernimmt

--- pull_data.ps1
+++ pull_data.ps1
@@ -5,6 +5,9 @@ param(
 Write-Host "Pulling latest Git changes..."
 git pull
 
+Write-Host "Syncing workspace to DVC state..."
+dvc checkout   # ← NEU: Stellt sicher dass Löschungen übernommen werden
+
 Write-Host "Pulling latest DVC data from remote..."
 dvc pull -r $remote
 
